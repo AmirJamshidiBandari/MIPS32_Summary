10cb6a342b578ef4994c2ba15be5ca6097c948a0
Amir <amiryee4@gmail.com>
2026-06-16 19:35:05 -0400

    Add multiplier and divider start/stop logic

--- RTL_Example/Mult_Div_Multicycle_Control.sv
+++ RTL_Example/Mult_Div_Multicycle_Control.sv
@@ -1,5 +1,3 @@
-import MIPS_Definitions::*;
-
 module Mult_Div_Multicycle_Control (
     input logic [6:0] cycle_div,
     input logic [5:0] cycle_mult,
@@ -24,26 +22,34 @@ always_comb begin
     Multicycle_HILO_Register_Enable_EX2 = 0;
     Multicycle_HILO_Select_EX2 = HILO_NONE;
 
+    // Multiplier start logic.
     if ((ALU_Control_EX2 == ALU_MULT) || (cycle_mult > 0 && cycle_mult < 6'd32)) begin
         Multiply = 1;
     end
+
+    // Divider start logic.
     if ((ALU_Control_EX2 == ALU_DIV) || (cycle_div > 0 && cycle_div < 6'd33)) begin
         Divide = 1;
     end
     
-
+    // Stall detection for multiplier.
     if (((Writeback_Control_ID == WB_HI) || (Writeback_Control_ID == WB_LO)) && (cycle_mult < 6'd32) && Multiply) begin
         stall_mult = 1;
     end
+
+    // Stall detection for divider.
     if (((Writeback_Control_ID == WB_HI) || (Writeback_Control_ID == WB_LO)) &&  (cycle_div < 6'd33) && Divide) begin
         stall_div = 1;
     end
 
+    // Multiplier stop logic.
     if (cycle_mult == 6'd32) begin
         Multiply = 0;
         Multicycle_HILO_Register_Enable_EX2 = 1;
         Multicycle_HILO_Select_EX2 = HILO_MULT;
     end
+
+    // Divider stop logic.
     if (cycle_div == 6'd33) begin
         Divide = 0;
         Multicycle_HILO_Register_Enable_EX2 = 1;
