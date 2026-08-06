cfc514ce145c2a3107c2f04122beef991a3a888a
Amir <amiryee4@gmail.com>
2026-08-06 19:32:02 -0400

    Update Control_Hazard_Unit.sv

--- RTL_Code/Control_Hazard_Unit.sv
+++ RTL_Code/Control_Hazard_Unit.sv
@@ -1,3 +1,7 @@
+/*
+Since instructions run in parallel, any branch or jump taken in EX1 stage will make the instructions in ID and IF stage garbage, because the next target instruction is at different address and the in-order flow begins at different address,
+therefore the garbage instructions must be flushed.
+*/
 module Control_Hazard_Unit (
     input logic Branch_EX1,
     input logic Zero_Flag_EX1,
@@ -9,7 +13,7 @@ module Control_Hazard_Unit (
     always_comb begin
         Flush_Branch = 0;
         Flush_ID = 0;
-        if ((Branch_EX1 && Zero_Flag_EX1) || Jump_EX1) begin
+        if ((Branch_EX1 && Zero_Flag_EX1) || Jump_EX1) begin // Flush garbage instructions.
             Flush_Branch = 1;
             Flush_ID = 1;
         end
