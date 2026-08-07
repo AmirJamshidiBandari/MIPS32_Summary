0800abf735cfbb054a5e0d61f6b2fe1aa23a0d78
Amir <amiryee4@gmail.com>
2026-08-06 21:06:37 -0400

    Update Jump_Concatenation.sv

--- RTL_Code/Jump_Concatenation.sv
+++ RTL_Code/Jump_Concatenation.sv
@@ -1,3 +1,4 @@
+
 module Jump_Concatenation (
     input logic [31:0] pc_adder_value_EX1,
     input logic Jump_EX1,
@@ -5,6 +6,6 @@ module Jump_Concatenation (
     output logic [31:0] jump_value_EX1
 ); 
 
-assign jump_value_EX1 = Jump_EX1 ? {pc_adder_value_EX1[31:28], target28_EX1} : 32'b0;
+    assign jump_value_EX1 = Jump_EX1 ? {pc_adder_value_EX1[31:28], target28_EX1} : 32'b0; // Jump to a target program counter address, the address is by concatenating shifted target value and upper 4 bits of next program counter value.
 
 endmodule
