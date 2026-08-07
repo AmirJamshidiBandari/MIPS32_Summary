565a4cfaf25d2845b265fe86d0bf608de96a3842
Amir <amiryee4@gmail.com>
2026-08-06 21:54:12 -0400

    Update Sign_Extender.sv

--- RTL_Code/Sign_Extender.sv
+++ RTL_Code/Sign_Extender.sv
@@ -2,5 +2,5 @@ module Sign_Extender (
     input logic [15:0] imm16_ID,
     output logic [31:0] sign_ext_imm32_ID
 );
-    assign sign_ext_imm32_ID = {{16{imm16_ID[15]}}, imm16_ID};
+    assign sign_ext_imm32_ID = {{16{imm16_ID[15]}}, imm16_ID}; // Extend the immediate based on the final bit.
 endmodule
