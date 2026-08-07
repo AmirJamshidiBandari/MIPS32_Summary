50d2abd517f07dc87cfa864b4f5e1f8065fa1212
Amir <amiryee4@gmail.com>
2026-08-06 21:33:35 -0400

    Add comment to PC_Adder module assignment
    Added comment to clarify the purpose of the assignment.

--- RTL_Code/PC_Adder.sv
+++ RTL_Code/PC_Adder.sv
@@ -2,6 +2,6 @@ module PC_Adder (
     input logic [31:0] pc,
     output logic [31:0] pc_adder_value
 );
-assign pc_adder_value = pc + 4;
+assign pc_adder_value = pc + 4; // Create the next program counter address.
 
 endmodule
