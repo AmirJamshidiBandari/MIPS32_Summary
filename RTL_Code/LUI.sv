578cb8352c4441b3470f3030296c5590e5ce9cb5
Amir <amiryee4@gmail.com>
2026-08-06 21:09:15 -0400

    Update LUI.sv

--- RTL_Code/LUI.sv
+++ RTL_Code/LUI.sv
@@ -2,5 +2,5 @@ module LUI_Unit (
     input logic [15:0] imm16_EX1,
     output logic [31:0] LUI_imm32_result_EX1
 );
-    assign LUI_imm32_result_EX1= (imm16_EX1 << 16);
+    assign LUI_imm32_result_EX1= (imm16_EX1 << 16); // The load upper immediate creates the upper 16 bits of a 32-bit value, the LUI and ORI instructions are mainly used to create large address and values.
 endmodule
