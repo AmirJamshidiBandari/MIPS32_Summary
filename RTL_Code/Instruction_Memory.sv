51c5c1ceca44d7d3e526f03a46738c9f2894e8be
Amir <amiryee4@gmail.com>
2026-08-06 20:58:48 -0400

    Update Instruction_Memory.sv

--- RTL_Code/Instruction_Memory.sv
+++ RTL_Code/Instruction_Memory.sv
@@ -1,3 +1,6 @@
+/*
+Read the instructions from Instructions.mem file, every cycle the program counter will point to a new address in instruction memory.
+*/
 module Instruction_Memory (
     input logic [31:0] pc,
     output logic [31:0] instruction_IF
@@ -5,7 +8,7 @@ module Instruction_Memory (
 integer i;
 logic [31:0] memory [31:0];
 initial begin
-    $readmemh("/mnt/c/Users/YourPC/python/Instructions.mem", memory);
+    $readmemh("/mnt/c/Users/YourPC/python/Instructions.mem", memory); // This depends on where your RTL folder is placed, this is a example of RTL folder in WSL.
     $display("----------------INSTRUCTION MEMORY----------------\n");
     for (i = 0; i < 32; i = i + 1) begin
         $display("Instruction %0d = %08H", i, memory[i]);
