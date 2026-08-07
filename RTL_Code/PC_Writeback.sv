383fd254c135139a6400b7c3b07d713eacd08b36
Amir <amiryee4@gmail.com>
2026-08-06 21:37:22 -0400

    Update PC_Writeback.sv

--- RTL_Code/PC_Writeback.sv
+++ RTL_Code/PC_Writeback.sv
@@ -1,3 +1,6 @@
+/*
+This is next program counter multiplexer, it either picks between jump target, branch target, or normal incremented program counter, the output address is used by the program counter register.
+*/
 module PC_Writeback (
     input logic Jump_EX1,
     input logic Branch_EX1,
